c03367f963afd1de3eac7ec31d175f66e280a452
acltearr <burroughskaelen@gmail.com>
2021-04-06 18:00:01 -0400

    User Login/Registering working

--- RocketElevatorsCustomerPortal/Areas/Identity/IdentityHostingStartup.cs
+++ RocketElevatorsCustomerPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -20,7 +20,7 @@ namespace RocketElevatorsCustomerPortal.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("CustomerPortalDbContextConnection")));
 
-                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                     .AddEntityFrameworkStores<CustomerPortalDbContext>();
             });
         }
