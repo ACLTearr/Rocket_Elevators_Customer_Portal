c07a738a48c6d996c3546a1e3084f378db72bddd
acltearr <burroughskaelen@gmail.com>
2021-04-06 10:53:53 -0400

    Added Customers tabe to SQL DB

---
+++ RocketElevatorsCustomerPortal/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketElevatorsCustomerPortal.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
